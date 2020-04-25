f73662981c5a4ece3a3b3abf41483236399f2300
Sunyz <15900313259@163.com>
2020-04-26 00:01:20 +0800

    修改注册组件逻辑

--- src/single-spa-config.js
+++ src/single-spa-config.js
@@ -4,32 +4,29 @@ import axios from 'axios';
 /*
 * runScript：一个promise同步方法。可以代替创建一个script标签，然后加载服务
 * */
-const runScript = async (url) => {
-    return new Promise((resolve, reject) => {
-        const script = document.createElement('script');
-        script.src = url;
-        script.onload = resolve;
-        script.onerror = reject;
-        const firstScript = document.getElementsByTagName('script')[0];
-        firstScript.parentNode.insertBefore(script, firstScript);
-    });
-};
+const runScript = url => new Promise((resolve, reject) => {
+    console.log(url)
+    const script = document.createElement('script');
+    script.src = url;
+    script.onload = resolve;
+    script.onerror = reject;
+    const firstScript = document.getElementsByTagName('script')[0];
+    firstScript.parentNode.insertBefore(script, firstScript);
+})
 
-/*
-* getManifest：远程加载manifest.json 文件，解析需要加载的js
-* */
-const getManifest = (url, bundle) => new Promise(async (resolve) => {
-    const { data } = await axios.get(url);
-    const { entrypoints, publicPath } = data;
-    const assets = entrypoints[bundle].assets;
-    for (let i = 0; i < assets.length; i++) {
-        await runScript(publicPath + assets[i]).then(() => {
-            if (i === assets.length - 1) {
-                resolve()
-            }
+const getSubPath = (url, bundle = 'app') => {
+    return axios.get(url)
+        .then(res => {
+            let { data } = res,
+                { entrypoints, publicPath } = data,
+                assets = entrypoints[bundle].assets;
+            return assets.map(asset => publicPath + asset);
         })
-    }
-});
+        .catch(err => Promise.reject(err));
+}
+
+const loadAllScript = paths => Promise.all(paths.map(runScript))
+
 
 singleSpa.registerApplication( //注册微前端服务
     'app1',
@@ -38,11 +35,10 @@ singleSpa.registerApplication( //注册微前端服务
         // return 一个singleSpa 模块对象，模块对象来自于要加载的js导出
         // 如果这个函数不需要在线引入，只需要本地引入一块加载：
         // () => import('xxx/main.js')
-        let singleVue = null;
-        await getManifest('http://127.0.0.1:3000/manifest.json', 'app').then(() => {
-            singleVue = window.singleVue;
-        });
-        return singleVue;
+        let paths = await getSubPath('http://127.0.0.1:3000/manifest.json', 'app');
+        console.log(paths);
+        await loadAllScript(paths);
+        return window.singleVue;
     },
     location => location.pathname.startsWith('/app1') // 配置微前端模块前缀
 );
